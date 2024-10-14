fa21f871f4de822ff5bf09a8a7c99d396cd77315
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-10-14 15:34:26 +0800

    西元2024年10月14日 (週一) 15時34分26秒 CST

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  return (
-    <div>
-      Hello World.{" "}
-      <Link href="/about">
-        About
-      </Link>
-    </div>
-  );
+  const router = useRouter();
+
+  // Use useEffect to redirect on component mount
+  useEffect(() => {
+    router.push('https://blog.pulipuli.info/');
+  }, []);
+
+  // You can remove this content if you don't want anything displayed on the home page
+  // return (
+  //   <div>
+  //     {/* This content will not be displayed due to the redirect */}
+  //     Hello World.{" "}
+  //     <Link href="/about">
+  //       About
+  //     </Link>
+  //   </div>
+  // );
 }
